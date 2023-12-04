752859f462e6d6cb16461a4d78e5467b23618cb0
antonionicolau <antonio.tioypedro1234@gmail.com>
2023-12-04 23:51:02 +0100

    added instruction to add firebase for different env

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flavor/src/app_widget.dart';
 import 'package:flutter/material.dart';
 
+/// If you you want to add different Firebase project
+/// for each environment
+/// do the following:
+/// place your Google-service file into the correspondent Flavor
+/// under app/src/your_flavor_name
 void main() {
   runApp(const MyApp());
 }
